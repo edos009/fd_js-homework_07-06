6ce43baab470ff5f0fa90abafbeb3226258dc190
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-07 21:15:47 +0300

    Add new Example

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+//First Example
+const getUserInputNumber = function () {
+  return prompt("Enter your number: ");
+};
+
+const checkIsValueNotNumber = function (number) {
+  return number === null || number === "" || isNaN(number);
+};
+
+const guessHiddenNumber = function () {
+  let tryValue = 0;
+  while (true) {
+    const userInputNumber = getUserInputNumber();
+    if (checkIsValueNotNumber(userInputNumber)) {
+      alert("You didn't enter a number, please try again");
+      tryValue++;
+    } else {
+      if (Number(userInputNumber) > SECRIT_PIN) {
+        tryValue++;
+        alert("The number must be less, please try again");
+      } else if (Number(userInputNumber) < SECRIT_PIN) {
+        tryValue++;
+        alert("The number must be greater, please try again");
+      } else {
+        tryValue++;
+        alert("Congratulations, you guessed it in " + tryValue + " attempts");
+        break;
+      }
+    }
+  }
+};
+
+guessHiddenNumber();
+
+//Second Example
+/*
 const getUserInputNumber = function () {
   return prompt("Enter your number: ");
 };
@@ -14,10 +50,10 @@ const guessHiddenNumber = function () {
       alert("You didn't enter a number, please try again");
       tryValue++;
     } else {
-      if (userInputNumber > SECRIT_PIN) {
+      if (Number(userInputNumber) > SECRIT_PIN) {
         tryValue++;
         alert("The number must be less, please try again");
-      } else if (userInputNumber < SECRIT_PIN) {
+      } else if (Number(userInputNumber) < SECRIT_PIN) {
         tryValue++;
         alert("The number must be greater, please try again");
       } else {
@@ -30,3 +66,5 @@ const guessHiddenNumber = function () {
 };
 
 guessHiddenNumber();
+*/
+
